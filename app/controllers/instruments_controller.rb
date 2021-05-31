6bf847420a88fcaa1611224d6fec53e005366a31
saulofilho <saulosilvaf@gmail.com>
2021-05-31 17:51:35 -0300

    [feat] views and instrument controller

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,57 +1,63 @@
 class InstrumentsController < ApplicationController
-  before_action :set_instrument, only: %i[ show edit update destroy ]
+  before_action :set_instrument, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /instruments or /instruments.json
+  # GET /instruments
+  # GET /instruments.json
   def index
-    @instruments = Instrument.all
+    @instruments = Instrument.all.order("created_at desc")
   end
 
-  # GET /instruments/1 or /instruments/1.json
+  # GET /instruments/1
+  # GET /instruments/1.json
   def show
   end
 
   # GET /instruments/new
   def new
-    @instrument = Instrument.new
+    @instrument = current_user.instruments.build
   end
 
   # GET /instruments/1/edit
   def edit
   end
 
-  # POST /instruments or /instruments.json
+  # POST /instruments
+  # POST /instruments.json
   def create
-    @instrument = Instrument.new(instrument_params)
+    @instrument = current_user.instruments.build(instrument_params)
 
     respond_to do |format|
       if @instrument.save
-        format.html { redirect_to @instrument, notice: "Instrument was successfully created." }
+        format.html { redirect_to @instrument, notice: 'Instrument was successfully created.' }
         format.json { render :show, status: :created, location: @instrument }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @instrument.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /instruments/1 or /instruments/1.json
+  # PATCH/PUT /instruments/1
+  # PATCH/PUT /instruments/1.json
   def update
     respond_to do |format|
       if @instrument.update(instrument_params)
-        format.html { redirect_to @instrument, notice: "Instrument was successfully updated." }
+        format.html { redirect_to @instrument, notice: 'Instrument was successfully updated.' }
         format.json { render :show, status: :ok, location: @instrument }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @instrument.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /instruments/1 or /instruments/1.json
+  # DELETE /instruments/1
+  # DELETE /instruments/1.json
   def destroy
     @instrument.destroy
     respond_to do |format|
-      format.html { redirect_to instruments_url, notice: "Instrument was successfully destroyed." }
+      format.html { redirect_to instruments_url, notice: 'Instrument was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -62,8 +68,8 @@ class InstrumentsController < ApplicationController
       @instrument = Instrument.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    # Never trust parameters from the scary internet, only allow the white list through.
     def instrument_params
-      params.require(:instrument).permit(:brand, :model, :description, :condition, :finish, :title, :price)
+      params.require(:instrument).permit(:brand, :model, :description, :condition, :finish, :title, :price, :image)
     end
 end
